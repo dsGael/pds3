5526c269d29b086cd2946590f46939e5af6d5b94
Gael Borchardt <gaelborchardt@gmail.com>
2026-02-09 13:54:26 -0700

    si

--- app/src/main/java/com/example/appcomprayventa/Opciones_Login/ActivityLoginEmail.kt
+++ app/src/main/java/com/example/appcomprayventa/Opciones_Login/ActivityLoginEmail.kt
@@ -1,21 +1,24 @@
 package com.example.appcomprayventa.Opciones_Login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appcomprayventa.R
+import com.example.appcomprayventa.RegistroEmail
+import com.example.appcomprayventa.databinding.ActivityLoginEmailBinding
 
 class ActivityLoginEmail : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginEmailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_email)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding= ActivityLoginEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.TxtRegistrarse.setOnClickListener {
+            startActivity(Intent (this@ActivityLoginEmail, RegistroEmail::class.java))
         }
     }
 }
--- app/src/main/java/com/example/appcomprayventa/RegistroEmail.kt
+++ app/src/main/java/com/example/appcomprayventa/RegistroEmail.kt
@@ -1,20 +1,47 @@
 package com.example.appcomprayventa
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appcomprayventa.databinding.ActivityRegistroEmailBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistroEmail : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistroEmailBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_registro_email)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding= ActivityRegistroEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth= FirebaseAuth.getInstance()
+        progressDialog= ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.BtnRegistrar.setOnClickListener {
+            validarInfo()
         }
+
     }
+
+
+    private var email=""
+    private var password=""
+    private var r_password=""
+
+
+    private fun validarInfo() {
+        email=binding.EtEmail.text.toString().trim()
+        password=binding.EtPassword.text.toString().trim()
+        r_password=binding.EtRPassword.text.toString().trim()
+
+    }
+
+
 }
