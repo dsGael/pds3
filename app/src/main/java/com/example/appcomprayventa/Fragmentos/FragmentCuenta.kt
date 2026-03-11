24247b32ce6b4536c047d8621f38abda0ae0ca32
Gael Borchardt <gaelborchardt@gmail.com>
2026-03-11 12:38:59 -0700

    editar perfil

--- app/src/main/java/com/example/appcomprayventa/Fragmentos/FragmentCuenta.kt
+++ app/src/main/java/com/example/appcomprayventa/Fragmentos/FragmentCuenta.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.appcomprayventa.Constantes
 import com.example.appcomprayventa.OpcionesLogin
 import com.example.appcomprayventa.R
@@ -17,9 +18,6 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import kotlin.random.Random
-import com.bumptech.glide.Glide
-
 
 class FragmentCuenta : Fragment() {
 
@@ -45,19 +43,10 @@ class FragmentCuenta : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        val numeroAleatorio = Random.nextInt(1, 4)
-        val nombreImagen="perfil_$numeroAleatorio"
-        val idImagen = resources.getIdentifier(nombreImagen, "drawable", mContext.packageName)
-        if (idImagen != 0) {
-            binding.TvPerfil.setImageResource(idImagen)
-        } else {
-            binding.TvPerfil.setImageResource(R.drawable.perfil)
-        }
+        firebaseAuth = FirebaseAuth.getInstance()
 
         leerInfo()
 
-        firebaseAuth = FirebaseAuth.getInstance()
         binding.BtnCerrarSesion.setOnClickListener {
             firebaseAuth.signOut()
             startActivity(Intent(mContext, OpcionesLogin::class.java))
@@ -67,32 +56,33 @@ class FragmentCuenta : Fragment() {
 
     private fun leerInfo() {
         val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
-        ref.child("${firebaseAuth.uid}")
-            .addValueEventListener(object: ValueEventListener{
+        ref.child(firebaseAuth.uid!!)
+            .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
-                    val nombres="${snapshot.child("nombres").value}"
-                    val email="${snapshot.child("email").value}"
-                    val imagen="${snapshot.child("urlImagenPerfil").value}"
-                    val f_nac="${snapshot.child("fecha_nac").value}"
-                    var tiempo="${snapshot.child("tiempo").value}"
-                    val telefono="${snapshot.child("telefono").value}"
-                    val codTelefono="${snapshot.child("codigoTelefono").value}"
-                    val proveedor="${snapshot.child("proveedor").value}"
+                    val nombre = "${snapshot.child("nombres").value}"
+                    val email = "${snapshot.child("email").value}"
+                    val imagen = "${snapshot.child("urlImagenPerfil").value}"
+                    val f_nac = "${snapshot.child("fecha_nac").value}"
+                    var tiempo = "${snapshot.child("tiempo").value}"
+                    val telefono = "${snapshot.child("telefono").value}"
+                    val codTelefono = "${snapshot.child("codigoTelefono").value}"
+                    val proveedor = "${snapshot.child("proveedor").value}"
 
-                    val cod_tel=codTelefono+telefono
+                    val cod_tel = codTelefono + telefono
 
-                    if(tiempo=="null"){
-                        tiempo="0"
+                    if (tiempo == "null"){
+                        tiempo = "0"
                     }
-                    val for_tiempo=Constantes.obtenerFecha(tiempo.toLong())
 
-                    binding.TvEmail.text=email
-                    binding.TvNombres.text=nombres
-                    binding.TvMiembro.text=for_tiempo
-                    binding.TvNacimiento.text=f_nac
-                    binding.TvTelefono.text=cod_tel
+                    val for_tiempo = Constantes.obtenerFecha(tiempo.toLong())
 
-                    try{
+                    binding.TvNombres.text = nombre
+                    binding.TvEmail.text = email
+                    binding.TvNacimiento.text = f_nac
+                    binding.TvMiembro.text = for_tiempo
+                    binding.TvTelefono.text = cod_tel
+
+                    try {
                         Glide.with(mContext)
                             .load(imagen)
                             .placeholder(R.drawable.perfil)
@@ -101,26 +91,20 @@ class FragmentCuenta : Fragment() {
                         Toast.makeText(mContext, "${e.message}", Toast.LENGTH_SHORT).show()
                     }
 
-                    if(proveedor=="Email"){
-                        val esVerificado=firebaseAuth.currentUser!!.isEmailVerified
-                        if(esVerificado){
-                            binding.TvEstadoCuenta.text="Verificado"
-
+                    if (proveedor == "Email"){
+                        val esVerificado = firebaseAuth.currentUser!!.isEmailVerified
+                        if (esVerificado){
+                            binding.TvEstadoCuenta.text = "Verificado"
+                        }else{
+                            binding.TvEstadoCuenta.text = "No verificado"
                         }
-                        else{
-                            binding.TvEstadoCuenta.text="No Verificado"
-                        }
-
-                    }else{
-                        binding.TvEstadoCuenta.text="Verificado"
+                    } else {
+                        binding.TvEstadoCuenta.text = "Verificado"
                     }
-
-
                 }
-                override fun onCancelled(error: DatabaseError) {}
+
+                override fun onCancelled(snapshot: DatabaseError) {
+                }
             })
-
-
     }
 }
-
