e1c7409f56577721fb352133e6c94b992d4621cc
Gael Borchardt <gaelborchardt@gmail.com>
2026-03-18 12:40:22 -0700

    editar perfil

--- app/src/main/java/com/example/appcomprayventa/EditarPerfil.kt
+++ app/src/main/java/com/example/appcomprayventa/EditarPerfil.kt
@@ -2,12 +2,21 @@ package com.example.appcomprayventa
 
 import android.app.ProgressDialog
 import android.os.Bundle
+import android.view.Menu
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.appcomprayventa.databinding.ActivityEditarPerfilBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class EditarPerfil : AppCompatActivity() {
     private lateinit var binding: ActivityEditarPerfilBinding
@@ -17,8 +26,102 @@ class EditarPerfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityEditarPerfilBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_editar_perfil)
+        setContentView(binding.root)
+
+        firebaseAuth= FirebaseAuth.getInstance()
+        progressDialog= ProgressDialog(this)
+        progressDialog.setTitle("Por favor espere")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        cargarInfo()
+
+        binding.FABCambiarImg.setOnClickListener {
+            selec_imagen_de()
+        }
+
 
 
     }
+
+    private fun cargarInfo(){
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child("${firebaseAuth.uid}")
+            .addValueEventListener(object : ValueEventListener {
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val nombres = "${snapshot.child("nombres").value}"
+                    val imagen = "${snapshot.child("urlImagenPerfil").value}"
+                    val f_nac = "${snapshot.child("fecha_nac").value}"
+                    val telefono = "${snapshot.child("telefono").value}"
+                    val codTelefono = "${snapshot.child("codigoTelefono").value}"
+
+                    binding.EtNombres.setText(nombres)
+                    binding.EtFNac.setText(f_nac)
+                    binding.EtTelefono.setText(telefono)
+
+                    try{
+                        Glide.with(applicationContext)
+                            .load(imagen)
+                            .placeholder(R.drawable.perfil)
+                            .into(binding.perfilIV)
+                    }catch (e: Exception){
+                        Toast.makeText(this@EditarPerfil, "${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+
+                    try{
+                        val codigo=codTelefono.replace("+","").toInt()
+                        binding.selectorCod.setCountryForPhoneCode(codigo)
+                    }catch (e: Exception){
+                        Toast.makeText(this@EditarPerfil, "${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+
+
+                }
+
+                override fun onCancelled(snapshot: DatabaseError) {
+
+                }
+
+
+
+            })
+    }
+
+    private fun selec_imagen_de(){
+        val popupMenu= PopupMenu(this,binding.FABCambiarImg)
+        popupMenu.menu.add(Menu.NONE,1,1,"Camara")
+        popupMenu.menu.add(Menu.NONE,2,2,"Galeria")
+
+        popupMenu.show()
+
+        popupMenu.setOnMenuItemClickListener { item ->
+            val itemId = item.itemId
+            if (itemId == 1) {
+                // camara
+            }else if (itemId == 2) {
+                // galeria
+            }
+            return@setOnMenuItemClickListener true
+        }
+
+    }
+
+    private val concederPermisoCamara= registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()){resultado->
+        var concedidoTodos=true
+        for (setConcede in resultado.values){
+            concedidoTodos=concedidoTodos && setConcede
+        }
+        if (concedidoTodos){
+            imagenCamara()
+        }else{
+            Toast.makeText(this, "No se concedieron los permisos", Toast.LENGTH_SHORT).show()
+
+
+        }
+    }
+}
+
+private fun imagenCamara() {
+    TODO("Not yet implemented")
 }
